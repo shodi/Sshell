b57efaa97ed84c093863fa9d55a2bd3ab9cb8b3f
Shodi <hikihenrique@yahoo.com>
2018-09-05 01:06:54 -0300

    first files

---
+++ Makefile
@@ -0,0 +1,5 @@
+Sshell: src/main.c
+	gcc src/main.c -o Sshell
+
+clean:
+	rm Sshell
---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+
+    char input[1000];
+
+    do {
+        scanf("%s", input);    
+    } while (1);
+    return 0;
+}
